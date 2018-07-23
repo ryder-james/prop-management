3010daff65033e41f678d8eda3fc4f84bbd23a42
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-23 11:12:28 -0600

    Auth user reducer

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -7,7 +7,6 @@ import SignUpForm from './signupForm';
 class SignUp extends Component {
     onSubmit = (fields) => {
         this.props.signUp(fields, () => {
-            console.log("navigate to dashboard");
             this.props.history.push("/dashboard");
         });
     }
---
+++ src/reducers/authReducer.js
@@ -0,0 +1,21 @@
+import { 
+    AUTHENTICATE_USER
+} from "../actions/types";
+
+const INIT_STATE = {
+    authenticated: false,
+    user: []
+}
+
+export default function(state = INIT_STATE, action) {
+    switch (action.type) {
+        case AUTHENTICATE_USER:
+            const { user } = action.payload;
+            return {
+                ...state,
+                user
+            }
+        default:
+            return state;
+    }
+}
