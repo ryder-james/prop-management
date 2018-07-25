609eda17cc87ac2d8b3d8c3b91c54526afdb0afc
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-25 09:47:24 -0600

    continued work on tabnav styles

--- src/components/tabnav.js
+++ src/components/tabnav.js
@@ -4,7 +4,11 @@ class TabNav extends Component {
     render() {
         return (
             <div className="tab-nav">
-            
+                {
+                    this.props.tabs.map((tab, index) => {
+                        return <a className="tab-nav__tab">{tab.title}</a>
+                    })
+                }
             </div>
         );
     }
