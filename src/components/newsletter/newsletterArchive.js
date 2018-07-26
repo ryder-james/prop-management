2236cd24cac56d2c78b39d26f52137ba685a46d5
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-26 10:25:28 -0600

    Fixed some styling issues and mapped newsletter object to latest

--- src/components/newsletter/newsletterArchive.js
+++ src/components/newsletter/newsletterArchive.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 function ArchiveItem({title, date}) {
     return (
-        <div className="archive-item archive-items__item">
+        <div className="archive-item">
             <div className="archive-item__title">{title}</div>
             <div className="archive-item__date">
                 { date.getMonth() + 1}/{ date.getDate() }/{ date.getFullYear() - 2000 }
