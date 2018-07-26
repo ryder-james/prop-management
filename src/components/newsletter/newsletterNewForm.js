67bfa2e1b3b119b027a7dbddffa1a0b1f92d9743
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-07-26 15:42:29 -0600

    built formImage

--- src/components/newsletter/newsletterNewForm.js
+++ src/components/newsletter/newsletterNewForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { reduxForm, Field } from "redux-form";
 
 import { FormTitle } from "../formTitle";
-import { FormInput, FormButton, FormTextArea } from "../formFields";
+import { FormInput, FormButton, FormTextArea, FormImage } from "../formFields";
 
 class NewNewsletterForm extends Component {
     render() {
@@ -44,6 +44,14 @@ class NewNewsletterForm extends Component {
                     type="button"
                     component={FormButton}
                     onClick={onCancel}
+                />
+                <Field
+                    className="new-newsletter-form__image"
+                    small={true}
+                    name="image"
+                    type="file"
+                    title="Image"
+                    component={FormImage}
                 />
             </form>
         );
