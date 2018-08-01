fae92d9cc39dffb51e8859ec3fc5ab5f75cba220
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-08-01 11:59:20 -0600

    Refactored styles for better organization

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -33,6 +33,7 @@ function main() {
             <Route path='/signup' component={SignUp}/>
 
             <Route path='/dashboard' component={Dashboard}/>
+            {/* <Route path='/dashboard' component={requireAuth(Dashboard)}/> */}
 
             <Route path='/newsletter/new' component={requireAuth(NewNewsletter)}/>
             <Route path='/newsletter/edit/:id' component={requireAuth(EditNewsletter)}/>
