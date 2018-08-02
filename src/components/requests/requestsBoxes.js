d673292fddcf93bd50fd5e36092c22555d588246
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-08-02 09:06:17 -0600

    Added and styled requests-box

--- src/components/requests/requestsBoxes.js
+++ src/components/requests/requestsBoxes.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import RequestsBox from './requestsBox';
 
 class RequestsBoxes extends Component {
     render() {
         return (
             <div className="requests-boxes">
-                pending
-                in progress
-                complete
+                <RequestsBox title="Pending" count={3}/>
+                <RequestsBox title="In Progress" count={1}/>
+                <RequestsBox title="Complete" count={0}/>
             </div>
         );
     }
