472f1f04602f2a49fe7af11c8b32f55b93b0fb76
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-08-02 09:50:19 -0600

    Updated requests-box styles based on active state

--- src/components/requests/requestsBox.js
+++ src/components/requests/requestsBox.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 
 class RequestsBox extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            active: false
+        }
+    }
+
     render() {
-        const { count, title } = this.props;
+        const { count, title, icon } = this.props;
         return (
-            <div className="requests-box">
+            <a onClick={() => this.setState({ active: !this.state.active})} className={`requests-box ${this.state.active ? "active" : ""}`}>
                 <div className="requests-box__count">{count}</div>
                 <div className="requests-box__title">{title}</div>
-                <div className="requests-box__point"/>
-            </div>
+                <div className={`requests-box__icon ${icon}`}/>
+                {this.state.active ? <div className="requests-box__point"/> : ""}
+            </a>
         );
     }
 }
--- src/components/requests/requestsBoxes.js
+++ src/components/requests/requestsBoxes.js
@@ -5,9 +5,9 @@ class RequestsBoxes extends Component {
     render() {
         return (
             <div className="requests-boxes">
-                <RequestsBox title="Pending" count={3}/>
-                <RequestsBox title="In Progress" count={1}/>
-                <RequestsBox title="Complete" count={0}/>
+                <RequestsBox title="Pending" count={3} icon="fas fa-plus"/>
+                <RequestsBox title="In Progress" count={1} icon="fas fa-plus"/>
+                <RequestsBox title="Complete" count={0} icon="fas fa-plus"/>
             </div>
         );
     }
