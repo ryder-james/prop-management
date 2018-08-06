1b2ecf0ecbaebbf8660a45c7993bda4f6612ef23
Skyler Tidwell <tidwell.skyler@yahoo.com>
2018-08-06 10:13:24 -0600

    built create new request action creator

--- src/actions/requests.js
+++ src/actions/requests.js
@@ -2,9 +2,25 @@ import {
     FETCH_REQUEST_TITLE
 } from './types';
 
+import axios from 'axios';
+import { ROOT_URL } from '../config';
+
 export function changeSelectedRequest(title) {
     return {
         type: FETCH_REQUEST_TITLE,
         payload: title
+    }
+}
+
+export function createNewRequest(newRequest, success) {
+    return function() {
+        axios.post(`${ROOT_URL}/requests/new`, newRequest)
+            then(response => {
+                console.log(response.data);
+                success();
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 }
